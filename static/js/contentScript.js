703150329afe760fdb2820deed77c1dd208b115b
samsiaw <samtsiaw@gmail.com>
2022-08-01 21:58:34 -0700

    simplify gesture recognition logic; revamp content-script

--- static/js/contentScript.js
+++ static/js/contentScript.js
@@ -1,109 +1,109 @@
-let abs = Math.abs;
-var trigger = "alt"; 
-const sensitivity = 15;
-const lim = 10;
-const gesture_hold_timer = 300; //Min Time between successful successive gestures
-const cookie = "gesDis";
-localStorage.removeItem(cookie);
-
-function keyPressed(event, key){
-    if (key === "alt"){
-        return event.altKey;
-    }
-}
 
 /* Sends messages to the extension's background scripts */
-function sendMess(str){
+function send_message(str){
     chrome.runtime.sendMessage({msg: str}, (response)=>{
        mess = response.msg;
        console.log(`script: ${mess}`);
     });
 }
 
-/* Gestures */
-/* Temporary hold on gestures to prevent same gesture firing multiple times */
-function canUseGes(){ // Can use gesture
-    if (localStorage[cookie] === undefined){
-        return true;
+//TODO Load from chrome.sync.storage
+var gesture_track = {
+    sensitivity: 15, 
+    points_limit: 8,
+    collect_points: false, 
+    gesture_enabled: true, 
+    status: {
+      first_point: undefined,
+      num_points: 0,
+      up: false,
+      down: false,
+      left: false,
+      right: false
+    },
+};
+
+const gesture_handler = (event, action_handler, mapping, gesture_track) => {
+    if (event.altKey) {
+      const x = event.screenX;
+      const y = event.screenY;
+      const sensitivity = gesture_track.sensitivity;
+      if (gesture_track.gesture_enabled){
+        const first_point = gesture_track.status.first_point;
+  
+        if (gesture_track.collect_points) {
+          if (gesture_track.status.num_points < gesture_track.points_limit){
+            // collect points
+            if (!first_point){
+              gesture_track.status.first_point = [x, y];
+            } else {
+              const dx = x - first_point[0];
+              const dy = y - first_point[1];
+              
+              if (Math.abs(dy) > sensitivity){
+                  if (dy < 0){
+                      gesture_track.status.up = true;
+                  } else if (dy > 0){
+                    gesture_track.status.down = true;
+                  }
+              }
+              if (Math.abs(dx) > sensitivity){
+                  if (dx < 0) {
+                    gesture_track.status.left = true;
+                  } else if (dx > 0) {
+                    gesture_track.status.right = true;
+                  }
+              }
+            }
+            gesture_track.status.num_points++;
+  
+          } else {
+            // Recognize gesture and reset tracking information
+            gesture_track.collect_points = false;
+            gesture_track.gesture_enabled = true;
+            const {up, down, left, right} = gesture_track.status;
+            
+            let str = "ms";
+            if (left) str += "L";
+            else if (right) str += "R";
+  
+            if (up) str += "U";
+            else if (down) str += "D";
+  
+            gesture_track.status.first_point = undefined;
+            gesture_track.status.up = false;
+            gesture_track.status.down = false;
+            gesture_track.status.left = false;
+            gesture_track.status.right = false;
+            gesture_track.status.num_points = 0;
+  
+            action_handler(str, mapping);
+          }
+  
+        } else {
+            // Start Tracking / Collecting Points to recognize gesture
+            gesture_track.collect_points = true;
+            gesture_track.gesture_enabled = true;
+            gesture_track.status.first_point = [x, y];
+        }
+      }
     }
-    return false;
-}
-
-function disGes(){ // Disable Gesture [Sets gesture tick and a timeout to remove the tick]
-    localStorage[cookie] = true;
-    setTimeout(()=>{localStorage.removeItem(cookie)}, gesture_hold_timer);
-}
-
+};
+  
+function action_handler(which_gesture, mapping_list){
+    const str_to_gesture = {
+        "msR": 5,
+        "msL": 4,
+        "msD": 0,
+        "msU": 1,
+        "msLD": 6,
+        "msRD": 3,
+        "msLU": 7,
+        "msRU": 2,
+    };
+    // TODO: Do something with the provided gesture
+};
 
 document.addEventListener("mousemove", (event)=>{
-    
-    if (keyPressed(event, trigger)){
-        
-        var relMoveX = event.movementX;
-        var relMoveY = event.movementY;
-
-        if (canUseGes()){
-            
-            if (abs(relMoveX)>sensitivity && abs(relMoveY)<lim){
-                if (relMoveX > 0){ // ms Right, ms ==> mouse
-                    disGes();
-                    
-                    sendMess("msR");
-                    return;
-                }
-
-                 else { //ms Left
-                    disGes();
-                  
-                    sendMess("msL");
-                    return;
-                }
-        }
-            if (abs(relMoveY)>sensitivity && abs(relMoveX)<lim){
-                if (relMoveY < 0 ){ //ms Up
-                    disGes();
-               
-                    sendMess("msU");
-                    return;
-                }
-                else { //ms Down
-                    disGes();
-                   
-                    sendMess("msD");
-                    return;
-                }
-        }
-            if (abs(relMoveY)>sensitivity && abs(relMoveX)>sensitivity){
-                if (relMoveX < 0){ // Right to left
-                    if (relMoveY > 0){ // ms Diagonal Right to left Downwards
-                        disGes();
-                
-                        sendMess("msLD");
-                        return;
-                    } else { 
-                        disGes();
-                       
-                        sendMess("msLU");
-                        return;
-                    }
-                }
-                else { // left to right
-                    if (relMoveY < 0){ // ms Diagonal Left to Right Upwards
-                        disGes();
-                   
-                        sendMess("msRU");
-                        return;
-                    }
-                    else { // ms Diagonal Left to Right Downwards
-                        disGes();
-                    
-                        sendMess("msRD");
-                        return;
-                    }
-
-                }
-                
-            }
-        }
-    }
+    gesture_handler(event, action_handler, mapping, gesture_track);
 });
--- static/js/options.js
+++ static/js/options.js
@@ -24,7 +24,7 @@ const gesture_descriptions = [
 
 const key_descriptions = ["Alt"];
 
-const direction_chars_hex = [
+const icon_chars = [
   "&#x21D3",
   "&#x21D1",
   "&#x21D7",
@@ -34,14 +34,14 @@ const direction_chars_hex = [
   "&#x21D9",
   "&#x21D6",
 ];
+//TODO fix syncing mapping with vue app
 // Maps Gesture (array index) to Command
 const default_mapping = [0, 1, 4, 3, 5, 6, 8, 9];
 const current_mapping = default_mapping; // TODO: Load mappings from chrome.sync
 const app_mapping = current_mapping.map((cmd_idx, idx) => {
   return {
     gesture: gesture_descriptions[idx],
-    icon: direction_chars_hex[idx],
-    command: cmd_descriptions[cmd_idx],
+    icon: icon_chars[idx],
     cmd_idx,
   };
 });
@@ -50,19 +50,34 @@ var sensitivity = 15; // Load from chrome.sync
 const app = Vue.createApp({
   data() {
     return {
-      items: app_mapping,
+      items: current_mapping,
+      icon_chars,
       cmd_descriptions,
       key_descriptions,
+      gesture_descriptions,
       islight,
       sensitivity
     };
   },
-  mounted() {
-    // window.customVue = this;
-  },
   methods: {
     changetheme: function(){
       this.islight = !this.islight;
+    },
+    changemapping: function(gestureIdx, newCmdIdx){
+      if (this.items[gestureIdx]){
+        this.items[gestureIdx].cmd_idx = newCmdIdx;
+        // TODO: update value in chrome.sync
+      }
+    },
+    getMappingAsList: function(){
+      return this.items;
+      // idx => Gesture idx, Value => Command idx
+      // return this.items.map((item) => item.cmd_idx);
+    }
+  },
+  watch : {
+    sensitivity : function(newVal,oldVal){
+      this.sensitivity = parseInt(newVal);
     }
   },
   computed: {
@@ -70,7 +85,7 @@ const app = Vue.createApp({
       return this.islight ? 'light' : 'dark';
     },
     changesensitivity(){
-      sensitivity = this.sensitivity;
+      sensitivity = parseInt(this.sensitivity);
     }
   }
 });
@@ -82,16 +97,20 @@ app.component("custom-select", {
     <option 
       v-for="(cmd, idx) in (istrigger ? this.$parent.key_descriptions : this.$parent.cmd_descriptions)" 
       :value="idx"
-      :theme="themestr">{{cmd}}</option>
+      :theme="themestr" v-text="cmd"></option>
   </select>`,
-  props: ["cmdidx", "istrigger"],
+  props: ["cmdidx", "istrigger", "gestureidx"],
   data() {
     return {
       selectedIdx: this.cmdidx,
       isTrigger: this.istrigger,
     };
   },
-  methods: {},
+  watch:{
+    selectedIdx: function(newCmdIdx, oldVal){
+      this.$parent.changemapping(this.gestureidx, newCmdIdx);
+    }
+  },
   computed: {
     themestr(){
       return this.$parent.themestr;
@@ -104,16 +123,10 @@ app.component("themer", {
   <input type="checkbox" id="themer" v-model.lazy="checked" @change="changetheme">
   <label for="themer">{{ descriptor }}</label> 
   `,
-  data() {
-    return {
-    };
-  },
   methods: {
     changetheme: function () {
       this.$parent.changetheme();
     },
-  },
-  mounted() {
   },
   computed: {
     descriptor() {
@@ -127,10 +140,12 @@ var gesture_track = {
   sensitivity: 15, 
   gesture_pause_time: 100, 
   point_collection_time: 100, 
+  points_limit: 8,
   collect_points: false, 
   gesture_enabled: true, 
   status: {
     first_point: undefined,
+    num_points: 0,
     up: false,
     down: false,
     left: false,
@@ -138,59 +153,15 @@ var gesture_track = {
   },
 };
 
-const disable_gesture = () => {
-  gesture_track.gesture_enabled = false;
-  setTimeout(() => {
-    gesture_track.gesture_enabled = true;
-  }, gesture_track.gesture_pause_time);
-};
-
-const evaluation_helper = (str) => {
-  action_handler(str);
-  disable_gesture();
-};
-
-const start_point_collection = () => {
-  gesture_track.collect_points = true;
-  gesture_track.gesture_enabled = true;
-
-  // stop points collection after timeout
-  setTimeout(() => {
-    gesture_track.collect_points = false;
-    gesture_track.gesture_enabled = true;
-    const {up, down, left, right} = gesture_track.status;
-    
-    if ((up && down) || (right && left) || !(up || down || left || right)) {
-      console.log("false positive");
-      evaluation_helper("");
-      return;
-    }
-    let str = "ms";
-    if (left) str += "L";
-    else if (right) str += "R";
-
-    if (up) str += "U";
-    else if (down) str += "D";
-
-    gesture_track.status.first_point = undefined;
-    gesture_track.status.up = false;
-    gesture_track.status.down = false;
-    gesture_track.status.left = false;
-    gesture_track.status.right = false;
-    evaluation_helper(str);
-
-  }, gesture_track.point_collection_time);
-};
-
-const gesture_handler = (event, action_handler) => {
+const gesture_handler = (event, action_handler, mapping, sensitivity) => {
   if (event.altKey) {
     const x = event.screenX;
     const y = event.screenY;
 
-    if (gesture_track.gesture_enabled){
+    if (gesture_track.collect_points){
       const first_point = gesture_track.status.first_point;
 
-      if (gesture_track.collect_points){
+      if (gesture_track.status.num_points < gesture_track.points_limit){
         // collect points
         if (!first_point){
           gesture_track.status.first_point = [x, y];
@@ -213,15 +184,37 @@ const gesture_handler = (event, action_handler) => {
               }
           }
         }
-      } else {
-        start_point_collection();
-        gesture_track.status.first_point = [x, y];
+        gesture_track.status.num_points++;
+      } else { // Done collecting points
+        // Recognize gesture and reset tracking information
+        gesture_track.collect_points = false;
+        const {up, down, left, right} = gesture_track.status;
+        
+        let str = "ms";
+        if (left) str += "L";
+        else if (right) str += "R";
+
+        if (up) str += "U";
+        else if (down) str += "D";
+
+        gesture_track.status.first_point = undefined;
+        gesture_track.status.up = false;
+        gesture_track.status.down = false;
+        gesture_track.status.left = false;
+        gesture_track.status.right = false;
+        gesture_track.status.num_points = 0;
+
+        action_handler(str, mapping);
       }
+    } else {
+      // Start Tracking / Collecting Points to recognize gesture
+      gesture_track.collect_points = true;
+      gesture_track.status.first_point = [x, y];
     }
   }
 };
 
-const action_handler = (which_gesture) =>{
+const action_handler = (which_gesture, mapping_list) =>{
   const str_to_gesture = {
     "msR": 5,
     "msL": 4,
@@ -235,9 +228,11 @@ const action_handler = (which_gesture) =>{
   if (str_to_gesture[which_gesture] !== undefined) {
     const pg_ges = document.getElementById("playground-gesture");
     const pg_cmd = document.getElementById("playground-command");
-    const mapping = current_mapping;
     pg_ges.innerHTML = gesture_descriptions[str_to_gesture[which_gesture]];
-    pg_cmd.innerHTML = cmd_descriptions[mapping[str_to_gesture[which_gesture]]];
+    pg_cmd.innerHTML = cmd_descriptions[mapping_list[str_to_gesture[which_gesture]]];
+  } 
+  else {
+    console.log("action_handler: unknown gesture code");
   }
 };
 
@@ -258,7 +253,7 @@ app.component("playground", {
   }, 
   mounted(){
     document.getElementById("playground").addEventListener("mousemove", (ev) => {
-      gesture_handler(ev, action_handler);
+      gesture_handler(ev, action_handler, this.$parent.getMappingAsList(), this.$parent.sensitivity);
     });
   }
 });
